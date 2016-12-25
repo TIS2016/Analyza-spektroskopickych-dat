e9cf43d828cf0e0ce1903d2cf7dd25b8a744a4dc
Filip <filo.kovac@gmail.com>
2016-12-25 21:00:04 +0100

    [Source Code] rename Spline param pair members

--- Sources/Internal/Transformations/Spline.h
+++ Sources/Internal/Transformations/Spline.h
@@ -5,10 +5,10 @@ namespace DataAnalysis { namespace Transformations {
 
 	template <class BaseType = double> class CubicSplineParamPair {
 	public:
-		BaseType a, b;
+		BaseType x, fx;
 
-		CubicSplineParamPair() : a( 0 ), b( 0 ) {};
-		CubicSplineParamPair( __in const BaseType &aa, __in const BaseType &bb ) : a( aa ), b( bb ) {};
+		CubicSplineParamPair() : x( 0 ), fx( 0 ) {};
+		CubicSplineParamPair( __in const BaseType &a, __in const BaseType &b ) : x( aa ), fx( bb ) {};
 
 	};
 
@@ -18,35 +18,26 @@ namespace DataAnalysis { namespace Transformations {
 			mType = FT_SPLINE_CUBIC;
 		};
 
-		void Initialize( __in const Buffer<CubicSplineParamPair<BaseType>> &params ) {
-			mParameterCount = params.Length();
-			mParameters.Set( params );
-			mInitialized = true;
-		}
+		void Initialize( __in const size_t pointCount, __in_ecount( count * 2 ) const BaseType *pSplinePoints ) {
+			InitializeVars( pointCount );
+			ConvertToPairs( pSplinePoints );
 
-		void Initialize( __in const vector<CubicSplineParamPair<BaseType>> &params ) {
-			mParameterCount = params.size();
-			mParameters.Set( params );
+			// TODO: init spline
+
 			mInitialized = true;
 		}
 
 		void Initialize( __in const Buffer<BaseType> &params ) {
 			// check if length is even
 			if ( ( params.Length() & 1 ) == 0 ) {
-				InitializeVars( params.Length() >> 1 );
-				ConvertToPairs( params.Ptr() );
-
-				mInitialized = true;
+				Initialize( params.Length() >> 1, params.Ptr() );
 			}
 		}
 
 		void Initialize( __in const vector<BaseType> &params ) {
 			// check if length is even
 			if ( ( params.size() & 1 ) == 0 ) {
-				InitializeVars( params.size() >> 1 );
-				ConvertToPairs( params.data() );
-
-				mInitialized = true;
+				Initialize( params.size() >> 1, params.data() );
 			}
 		}
 
@@ -55,20 +46,20 @@ namespace DataAnalysis { namespace Transformations {
 		}
 
 	protected:
-		size_t mParameterCount;
-		Buffer< CubicSplineParamPair<BaseType> > mParameters;
+		size_t mSplinePointCount;
+		Buffer< CubicSplineParamPair<BaseType> > mSplinePoints;
 
 	protected:
 
 		inline void InitializeVars( __in const size_t count ) {
-			mParameterCount = count;
-			mParameters.Allocate( count );
+			mSplinePointCount = count;
+			mSplinePoints.Allocate( count );
 		}
 
 		inline void ConvertToPairs( __in_ecount( mParameterCount * 2 ) const BaseType *pVals ) {
-			for ( size_t i = 0; i < mParameterCount; i++ ) {
-				mParameters[i].a = *pVals;
-				mParameters[i].b = *( pVals + 1 );
+			for ( size_t i = 0; i < mSplinePointCount; i++ ) {
+				mSplinePoints[i].x = *pVals;
+				mSplinePoints[i].fx = *( pVals + 1 );
 				pVals += 2;
 			}
 		}
