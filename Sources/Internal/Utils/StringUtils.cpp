37a2a4e3fd5ec278b706981f6f2970a950027b92
Filip <filo.kovac@gmail.com>
2016-12-30 18:10:47 +0100

    [Source Code] fix weird chars in input strings

--- Sources/Internal/Utils/StringUtils.cpp
+++ Sources/Internal/Utils/StringUtils.cpp
@@ -5,7 +5,7 @@ namespace DataAnalysis { namespace Utils {
 
 	void LStrToStr ( __in const LStrPtr pIn, __out std::string &outStr ) {
 		const char *pSrc = reinterpret_cast<const char *>( pIn->str );
-		outStr.assign( pSrc );
+		outStr.assign( pSrc, pIn->cnt );
 	}
 
 } }
